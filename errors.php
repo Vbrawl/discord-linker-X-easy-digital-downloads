9760b2ce647770a98980fb5a868a97b3d328c5c9
Vbrawl <greninjagr@gmail.com>
2023-02-22 20:00:52 +0200

    Moved all errors to another file.

---
+++ errors.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+
+
+
+
+function dlxedd_error_INCORRECT_PRODUCT_ID_TYPE() {
+    return new WP_Error("INCORRECT_PRODUCT_ID_TYPE", "Product ID must be integer!");
+}
+
+
+function dlxedd_error_PRODUCT_NOT_FOUND() {
+    return new WP_Error("PRODUCT_NOT_FOUND", "There are no products with this ID!");
+}
+
+
+function dlxedd_error_PRODUCT_NOT_IN_CART($product_id) {
+    return new WP_Error("PRODUCT_NOT_IN_CART", "Your cart doesn't contain a product with this ID!", array("id" => $product_id));
+}
