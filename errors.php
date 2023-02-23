ac7d1515e5ddd0f6f234056c273592996ea96b2e
Vbrawl <greninjagr@gmail.com>
2023-02-23 18:08:57 +0200

    Added INCORRECT_DATE_TYPE error.

--- errors.php
+++ errors.php
@@ -19,3 +19,7 @@ function dlxedd_error_PRODUCT_NOT_FOUND() {
 function dlxedd_error_PRODUCT_NOT_IN_CART($product_id) {
     return new WP_Error("PRODUCT_NOT_IN_CART", "Your cart doesn't contain a product with this ID!", array("id" => $product_id));
 }
+
+function dlxedd_error_INCORRECT_DATE_TYPE() {
+    return new WP_Error("INCORRECT_DATE_TYPE", "A Date must be an epoch value!");
+}
